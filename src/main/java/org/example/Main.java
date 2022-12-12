26a2baaabd22e964fc58f3e89dd0972d2ab1e84b
Rafik Tekfa <rafik.tekfa@etu.univ-lehavre.fr>
2022-12-12 14:32:41 +0100

    visualiser le graphe

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.DefaultGraph;
+import org.graphstream.stream.file.FileSourceEdge;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        public static void main(String[] args) {
+            System.setProperty("org.graphstream.ui", "swing");
+            Graph mongraphe = new DefaultGraph("Graphe");
+            FileSourceEdge fs = new FileSourceEdge();
+
+            fs.addSink(mongraphe);
+            mongraphe.display();
+
+            try {
+                fs.readAll("/home/c2i/Bureau/TP-mesureReseau/tp-mri/fichier.txt");
+            } catch (IOException e) {
+                System.out.println(e);
+
+            }
+        }
     }
-}

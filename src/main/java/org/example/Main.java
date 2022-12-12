e2f2271fbef1f2b48712443fee4d2ce036f2a412
Rafik Tekfa <rafik.tekfa@etu.univ-lehavre.fr>
2022-12-12 14:58:18 +0100

    quelques mesures de base le nombre de noeud et le degre moyen et le coefficient de clustring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.DefaultGraph;
 import org.graphstream.stream.file.FileSourceEdge;
+import static org.graphstream.algorithm.Toolkit.*;
 
 import java.io.IOException;
 
@@ -21,5 +22,9 @@ public class Main {
                 System.out.println(e);
 
             }
+            System.out.println("Le nombre de noeuds:" + mongraphe.getNodeCount());
+            System.out.println("Le degré moyen :" + averageDegree(mongraphe));
+            System.out.println("Le coefficient de clustering:" + averageClusteringCoefficient(mongraphe));
+
         }
     }

09db71f9a9225a724d2186b7f055e5b1267c88b8
Rafik Tekfa <rafik.tekfa@etu.univ-lehavre.fr>
2022-12-12 15:06:07 +0100

    coefficient de clustering pour un réseau aléatoire de la même taille et du même degré moyen

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,11 +10,11 @@ import java.io.IOException;
 public class Main {
         public static void main(String[] args) {
             System.setProperty("org.graphstream.ui", "swing");
-            Graph mongraphe = new DefaultGraph("Graphe");
+            Graph graphe1 = new DefaultGraph("Graphe");
             FileSourceEdge fs = new FileSourceEdge();
 
-            fs.addSink(mongraphe);
-            mongraphe.display();
+            fs.addSink(graphe1);
+            graphe1.display();
 
             try {
                 fs.readAll("/home/c2i/Bureau/TP-mesureReseau/tp-mri/fichier.txt");
@@ -22,9 +22,9 @@ public class Main {
                 System.out.println(e);
 
             }
-            System.out.println("Le nombre de noeuds:" + mongraphe.getNodeCount());
-            System.out.println("Le degré moyen :" + averageDegree(mongraphe));
-            System.out.println("Le coefficient de clustering:" + averageClusteringCoefficient(mongraphe));
-
+            System.out.println("Le nombre de noeuds:" + graphe1.getNodeCount());
+            System.out.println("Le degré moyen :" + averageDegree(graphe1));
+            System.out.println("Le coefficient de clustering:" + averageClusteringCoefficient(graphe1));
+            System.out.println("Le coefficient de clustering pour un réseau aléatoire de la même taille et du même degré moyen :" + averageDegree(graphe1) / graphe1.getNodeCount());
         }
     }
